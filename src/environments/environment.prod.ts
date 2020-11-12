f04d33c1a05ecd6fadc1b80194db7dddb3d8c612
Siddharth Behra <Siddharth.Behra@neudesic.com>
2020-11-12 18:01:27 +0530

    Added express and made changes in package json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyB9IjhhLC7T2faTLaRsYV_TJyI3eQ7N9-0",
+    authDomain: "github-searcher-295102.firebaseapp.com",
+    databaseURL: "https://github-searcher-295102.firebaseio.com",
+    projectId: "github-searcher-295102",
+    storageBucket: "github-searcher-295102.appspot.com",
+    messagingSenderId: "543257132017",
+    appId: "1:543257132017:web:b643718cc1f92eb29b2250"
+  },
 };
